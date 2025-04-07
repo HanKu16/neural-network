a5888c35066467e72a75a9b1182928a76730a7a5
Jakub Pietrzykowski <j.pietrzykowski17@gmail.com>
2025-04-07 19:33:45 +0200

    Add new doc comments.

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -13,18 +13,6 @@ class NeuralNetwork:
         self.weight_init_algorithm = None
         self.weights = None
 
-    def setup_for_learning(self, number_of_inputs: int,
-                           number_of_outputs: int,
-                           number_of_neurons_in_each_hidden_layer: list[int],
-                           learning_rate: float) -> None:
-        self.number_of_inputs = number_of_inputs
-        self.number_of_outputs = number_of_outputs
-        self.number_of_neurons_in_each_layer = number_of_neurons_in_each_hidden_layer
-        self.number_of_neurons_in_each_layer.append(number_of_outputs)
-        self.activation_function = ActivationFunctions.sigmoid_function
-        self.learning_rate = learning_rate
-        self.weight_init_algorithm = "xavier"
-
     def process(self, inputs: list[float]) -> list[float]:
         """Process input vector given to the network and return output vector
         which is product the network and inputs vector.
@@ -48,24 +36,6 @@ class NeuralNetwork:
                 output_from_layer.append(neuron_output)
             inputs = output_from_layer[:]
         return output_from_layer
-
-    def _process_for_learning(self, inputs_from_sample: list[float]) -> (list[list[float]], list[list[float]]):
-        outputs_from_all_layers: list[list[float]] = []
-        inputs_for_all_layers: list[list[float]] = []
-        number_of_layers: int = len(self.weights)
-        input_vector_with_bias = self._get_input_vector_with_bias_input(inputs_from_sample)
-        for layer_index in range(number_of_layers):
-            output_from_layer = []
-            inputs_for_all_layers.append(input_vector_with_bias[:])
-            number_of_neurons_in_layer: int = self.number_of_neurons_in_each_layer[layer_index]
-            for neuron_index in range(number_of_neurons_in_layer):
-                weights_for_neuron: list[float] = self.weights[layer_index][neuron_index]
-                u: float = self._calculate_u(input_vector_with_bias, weights_for_neuron)
-                neuron_output: float = self.activation_function(u)
-                output_from_layer.append(neuron_output)
-            outputs_from_all_layers.append(output_from_layer)
-            input_vector_with_bias = self._get_input_vector_with_bias_input(output_from_layer[:])
-        return outputs_from_all_layers, inputs_for_all_layers
 
     def learn(self, sample: TrainingData):
         outputs_from_all_layers, inputs_for_all_layers = self._process_for_learning(sample.inputs)
@@ -104,6 +74,152 @@ class NeuralNetwork:
             self.weights = weight_initializer.random_init()
         else:
             self.weights = weight_initializer.xavier_init()
+
+    def save_to_file(self, path_to_file: str) -> None:
+        """Read neural network from .txt file at given path.
+
+        Args:
+            path_to_file (str): path where serialized object of neural network is saved
+
+        Raises:
+            FileNotFoundError: If given directory does not exist
+            PermissionError: If user does not have permission to access file or directory
+            IsADirectoryError: If 'path_to_file' is a directory not path to .txt file
+            OSError: If something goes wrong on low level operations
+        """
+        network_as_dict = {
+            "number_of_inputs": self.number_of_inputs,
+            "number_of_outputs": self.number_of_outputs,
+            "number_of_neurons_in_each_layer": self.number_of_neurons_in_each_layer,
+            "learning_rate": self.learning_rate,
+            "weight_init_algorithm": self.weight_init_algorithm,
+            "weights": self.weights
+        }
+        if self.activation_function == ActivationFunctions.jump_function:
+            network_as_dict["activation_function"] = "jump_function"
+        elif self.activation_function == ActivationFunctions.sigmoid_function:
+            network_as_dict["activation_function"] = "sigmoid_function"
+        network_json = json.dumps(network_as_dict, indent=4)
+        with open(path_to_file, "w") as json_file:
+            json_file.write(network_json)
+
+    @staticmethod
+    def read_from_file(path_to_file: str) -> "NeuralNetwork":
+        """Read neural network from .txt file at given path.
+
+        Args:
+            path_to_file (str): path where serialized object of neural network is saved
+
+        Raises:
+            FileNotFoundError: If no file is found at given 'path_to_file'
+            PermissionError: If user does not have permission to access file
+            IsADirectoryError: If 'path_to_file' is a directory not .txt file
+            json.JSONDecodeError: If data in the file is not valid JSON
+
+        Returns:
+            NeuralNetwork: Deserialized object of neural network.
+        """
+        with open(path_to_file, "r") as file:
+            data = json.load(file)
+            network = NeuralNetwork()
+            for key, value in data.items():
+                if hasattr(network, key):
+                    if key != "activation_function":
+                        setattr(network, key, value)
+                    else:
+                        if value == "jump_function":
+                            network.use_jump_activation_function()
+                        elif value == "sigmoid_function":
+                            network.use_sigmoid_activation_function()
+            return network
+
+
+    def setup_for_learning(self, number_of_inputs: int,
+                           number_of_outputs: int,
+                           number_of_neurons_in_each_hidden_layer: list[int],
+                           learning_rate: float) -> None:
+        """Set up neural network before learning process. Should not be called when
+        existing neural network instance was read from the file.
+
+        Args:
+            number_of_inputs (int): number of inputs for neural network based on data samples
+            number_of_outputs (int): number of outputs from neural network
+            number_of_neurons_in_each_hidden_layer (list[int]):
+                each number in the list specifies number of neuron in each hidden layer.
+                First number specifies number of neurons in input layer, second in the next
+                layer etc. If list is empty neural network will have 0 hidden layers.
+            learning_rate:
+                floating point number which have influence how fast weight of each neuron
+                changes, the bigger number the more rapid changes. It is recommended to
+                use learning rate in scope of (0, 1)
+        """
+        self.number_of_inputs = number_of_inputs
+        self.number_of_outputs = number_of_outputs
+        self.number_of_neurons_in_each_layer = number_of_neurons_in_each_hidden_layer
+        self.number_of_neurons_in_each_layer.append(number_of_outputs)
+        self.activation_function = ActivationFunctions.sigmoid_function
+        self.learning_rate = learning_rate
+        self.weight_init_algorithm = "xavier"
+
+    def use_jump_activation_function(self) -> None:
+        """Set jump function as activation function for neural network,
+        which outputs: 0 if argument is negative, otherwise 1.
+        Function is not differentiable so it can not be use with
+        backpropagation learning algorithm.
+        """
+        self.activation_function = ActivationFunctions.jump_function
+
+    def use_sigmoid_activation_function(self) -> None:
+        """Set sigmoid as activation function for neural network.
+        For sigmoid function output is always in scope (0, 1).
+        Learn more: https://en.wikipedia.org/wiki/Sigmoid_function.
+        """
+        self.activation_function = ActivationFunctions.sigmoid_function
+
+    def use_xavier_weight_initialization(self) -> None:
+        """Set xavier weight initialization. This initialization works
+        fine with activation functions like sigmoid, tanh etc.
+        """
+        self.weight_init_algorithm = "xavier"
+
+    def use_random_weight_initialization(self) -> None:
+        """Set random weight initialization. Each weight
+        in neural network is random number from scope <-1, 1>.
+        """
+        self.weight_init_algorithm = "random"
+
+    def _process_for_learning(self, inputs_from_sample: list[float]) -> (list[list[float]], list[list[float]]):
+        """Process given sample from 'inputs_from_sample'.
+
+        Args:
+            inputs_from_sample (list[float]): input vector from data sample
+
+        Returns:
+            (list[list[float]], list[list[float]]):
+                Elements in the value of first tuple are all the outputs given by each neuron
+                in the network for given sample 'inputs_from_sample'. To get output of
+                particular neuron refer by [layer_index][neuron_index].
+                Elements in the value of second tuple are all the inputs given to each neuron
+                in the network fir given sample. To get input given to particular weight
+                (input number) of neuron refer by [layer_index][weight_index]. Remember that
+                for each layer on [layer_index][0] there is bias input equal 1.
+        """
+        outputs_from_all_layers: list[list[float]] = []
+        inputs_for_all_layers: list[list[float]] = []
+        number_of_layers: int = len(self.weights)
+        input_vector_with_bias = self._get_input_vector_with_bias_input(inputs_from_sample)
+        for layer_index in range(number_of_layers):
+            output_from_layer = []
+            inputs_for_all_layers.append(input_vector_with_bias[:])
+            number_of_neurons_in_layer: int = self.number_of_neurons_in_each_layer[layer_index]
+            for neuron_index in range(number_of_neurons_in_layer):
+                weights_for_neuron: list[float] = self.weights[layer_index][neuron_index]
+                u: float = self._calculate_u(input_vector_with_bias, weights_for_neuron)
+                neuron_output: float = self.activation_function(u)
+                output_from_layer.append(neuron_output)
+            outputs_from_all_layers.append(output_from_layer)
+            input_vector_with_bias = self._get_input_vector_with_bias_input(output_from_layer[:])
+        return outputs_from_all_layers, inputs_for_all_layers
 
     def _adjust_weight(self, weight: float, delta: float, input_for_this_weight: float) -> float:
         """Calculate new weight based on backpropagation algorithm.
@@ -204,91 +320,6 @@ class NeuralNetwork:
         neuron_output = outputs_from_all_layers[layer_index][neuron_index]
         delta = delta * (-1) * neuron_output * (1 - neuron_output)
         return delta
-
-    def save_to_file(self, path_to_file: str) -> None:
-        """Read neural network from .txt file at given path.
-
-        Args:
-            path_to_file (str): path where serialized object of neural network is saved
-
-        Raises:
-            FileNotFoundError: If given directory does not exist
-            PermissionError: If user does not have permission to access file or directory
-            IsADirectoryError: If 'path_to_file' is a directory not path to .txt file
-            OSError: If something goes wrong on low level operations
-        """
-        network_as_dict = {
-            "number_of_inputs": self.number_of_inputs,
-            "number_of_outputs": self.number_of_outputs,
-            "number_of_neurons_in_each_layer": self.number_of_neurons_in_each_layer,
-            "learning_rate": self.learning_rate,
-            "weight_init_algorithm": self.weight_init_algorithm,
-            "weights": self.weights
-        }
-        if self.activation_function == ActivationFunctions.jump_function:
-            network_as_dict["activation_function"] = "jump_function"
-        elif self.activation_function == ActivationFunctions.sigmoid_function:
-            network_as_dict["activation_function"] = "sigmoid_function"
-        network_json = json.dumps(network_as_dict, indent=4)
-        with open(path_to_file, "w") as json_file:
-            json_file.write(network_json)
-
-    @staticmethod
-    def read_from_file(path_to_file: str) -> "NeuralNetwork":
-        """Read neural network from .txt file at given path.
-
-        Args:
-            path_to_file (str): path where serialized object of neural network is saved
-
-        Raises:
-            FileNotFoundError: If no file is found at given 'path_to_file'
-            PermissionError: If user does not have permission to access file
-            IsADirectoryError: If 'path_to_file' is a directory not .txt file
-            json.JSONDecodeError: If data in the file is not valid JSON
-
-        Returns:
-            NeuralNetwork: Deserialized object of neural network.
-        """
-        with open(path_to_file, "r") as file:
-            data = json.load(file)
-            network = NeuralNetwork()
-            for key, value in data.items():
-                if hasattr(network, key):
-                    if key != "activation_function":
-                        setattr(network, key, value)
-                    else:
-                        if value == "jump_function":
-                            network.use_jump_activation_function()
-                        elif value == "sigmoid_function":
-                            network.use_sigmoid_activation_function()
-            return network
-
-    def use_jump_activation_function(self) -> None:
-        """Set jump function as activation function for neural network,
-        which outputs: 0 if argument is negative, otherwise 1.
-        Function is not differentiable so it can not be use with
-        backpropagation learning algorithm.
-        """
-        self.activation_function = ActivationFunctions.jump_function
-
-    def use_sigmoid_activation_function(self) -> None:
-        """Set sigmoid as activation function for neural network.
-        For sigmoid function output is always in scope (0, 1).
-        Learn more: https://en.wikipedia.org/wiki/Sigmoid_function.
-        """
-        self.activation_function = ActivationFunctions.sigmoid_function
-
-    def use_xavier_weight_initialization(self) -> None:
-        """Set xavier weight initialization. This initialization works
-        fine with activation functions like sigmoid, tanh etc.
-        """
-        self.weight_init_algorithm = "xavier"
-
-    def use_random_weight_initialization(self) -> None:
-        """Set random weight initialization. Each weight
-        in neural network is random number from scope <-1, 1>.
-        """
-        self.weight_init_algorithm = "random"
 
     @staticmethod
     def _calculate_u(inputs: list[float], weights_for_neuron: list[float]) -> float:
